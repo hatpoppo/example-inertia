38487d48587e85775489b8a0badacba1a7cf9f22
hatpoppo <toshi.hat@gmail.com>
2025-11-15 09:08:07 +0900

    shadcn-vue button再追加

--- resources/js/components/ui/button/Button.vue
+++ resources/js/components/ui/button/Button.vue
@@ -3,7 +3,13 @@ import { Primitive } from "reka-ui";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from ".";
 
-const props = defineProps();
+const props = defineProps({
+  variant: { type: null, required: false },
+  size: { type: null, required: false },
+  class: { type: null, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false, default: "button" },
+});
 </script>
 
 <template>
